2f94dd6506ae0e52c98fd3ad193c6db47184a59d
Diogotb <digogo@gmail.com>
2023-10-23 13:29:01 -0300

    23/10

--- GUISerializacao/src/App.java
+++ GUISerializacao/src/App.java
@@ -1,5 +1,18 @@
+import javax.swing.SwingUtilities;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // SwingUtilities.invokeLater garante que a interface gráfica Swing seja criada na thread de eventos Swing,
+        // que é a thread apropriada para manipular a GUI. Isso evita problemas de concorrência.
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                // Cria uma instância da classe CadastroUsuarios, que é a classe principal da aplicação.
+                CadastroUsuarios app = new CadastroUsuarios();
+                app.pack();
+                // Torna a janela da aplicação visível para o usuário.
+                app.setVisible(true);
+            }
+        });
     }
 }
